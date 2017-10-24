d9deb276a26cd7b8aa1929c7d58a7f5595ec31ed
Dan Lindsay <dlindsay72@hotmail.com>
2017-10-23 20:11:57 -0400

    performSearch method done, locations display in tableView and clear if we change our search or clear the textField

--- Hitch/Controller/HomeVC.swift
+++ Hitch/Controller/HomeVC.swift
@@ -29,6 +29,7 @@ class HomeVC: UIViewController {
     var locationManager: CLLocationManager?
     var regionRadius: CLLocationDistance = 1000
     var tableView = UITableView()
+    var matchingItems: [MKMapItem] = [MKMapItem]()
     
     //MARK: - Methods
     
@@ -167,6 +168,28 @@ extension HomeVC: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
         centerMapBtn.fadeTo(alphaValue: 1.0, withDuration: 0.2)
     }
+    
+    func performSearch() {
+        matchingItems.removeAll()
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = destinationTextField.text
+        request.region = mapView.region
+        
+        let search = MKLocalSearch(request: request)
+        
+        search.start { (response, error) in
+            if error != nil {
+                print(error.debugDescription)
+            } else if response?.mapItems.count == 0 {
+                print("No results!")
+            } else {
+                for mapItem in response!.mapItems {
+                    self.matchingItems.append(mapItem as MKMapItem)
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
 }
 
 //MARK: - UITextFieldDelegate
@@ -207,13 +230,16 @@ extension HomeVC: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == destinationTextField {
-            // performSearch()
+            performSearch()
             view.endEditing(true)
         }
         return true
     }
     
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        matchingItems = []
+        tableView.reloadData()
+        
         centerMapOnUserLocation()
         return true
     }
@@ -241,7 +267,12 @@ extension HomeVC: UITextFieldDelegate {
 
 extension HomeVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: LOCATION_CELL)
+        let mapItem = matchingItems[indexPath.row]
+        cell.textLabel?.text = mapItem.name
+        cell.detailTextLabel?.text = mapItem.placemark.title
+        
+        return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -249,7 +280,7 @@ extension HomeVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return matchingItems.count
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
